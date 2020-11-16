83199ec57ab8824ab904811eeddfabe89ce17730
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-11-16 15:53:57 +0100

    Woho! Bot fungerar

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.getElementById('too-high-btn').addEventListener('click', function () {
     document.getElementById("counter").innerText = "Guesses: " + antalGissningar
     //vanliga uppgiften
     bigGuess = guess
-    guess = Math.floor((bigGuess + smolGuess)/2)
+    guess = Math.floor((bigGuess + smolGuess) / 2)
     setMessage(`Is it ${guess}?`)
 })
 
@@ -22,11 +22,11 @@ document.getElementById('too-low-btn').addEventListener('click', function () {
     document.getElementById("counter").innerText = "Guesses: " + antalGissningar
     //vanliga uppgiften
     smolGuess = guess
-    guess = Math.floor((bigGuess + smolGuess)/2)
+    guess = Math.floor((bigGuess + smolGuess) / 2)
     setMessage(`Is it ${guess}?`)
 })
 
-document.getElementById('is-correct-btn').addEventListener('click',function() {
+document.getElementById('is-correct-btn').addEventListener('click', function () {
     setMessage("Woho! :)")
 })
 
@@ -39,11 +39,26 @@ function start() {
     document.querySelector('main').style.display = 'block'
     guess = 50
     setMessage(`Is it ${guess}?`)
+    startBot()
 }
-
-document.getElementById("counter").innerText = "Guesses: " + antalGissningar
 
 document
     .getElementById('start-btn')
     .addEventListener('click', start)
 
+//fördjupning
+document.getElementById("counter").innerText = "Guesses: " + antalGissningar
+
+function startBot() {
+    const secretGuess = Math.floor(Math.random() * 100) + 1
+    console.log(`starting bot with ${secretGuess}...`)
+    setInterval(function () {
+        if (secretGuess === guess) {
+            document.getElementById('is-correct-btn').click()
+        } if (secretGuess > guess) {
+            document.getElementById('too-low-btn').click()
+        } if (secretGuess < guess) {
+            document.getElementById('too-high-btn').click() 
+        }
+    }, 1000)
+}
